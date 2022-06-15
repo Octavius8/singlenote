02215c45c656cae0c939896585cdcc7d5bb455d4
FlutLab.io <>
2022-06-15 12:18:23 +0000

    Autosave

--- lib/model/_user.dart
+++ lib/model/_user.dart
@@ -1,16 +1,23 @@
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import '../model/_log.dart';
 
 class User {
   Future<bool> saveUserData() async {
-    final directory = await getApplicationSupportDirectory();
-    String path = directory.path;
-    final file = await File('$path/userprofile.txt');
-    String data = "Hey hey";
-    // Write the file
-    file.writeAsString(data);
+    String logPrefix = "User | saveUserData";
+    Log log = new Log();
+    log.debug(logPrefix, "Checking if this is a browser: " + kIsWeb.toString());
+    if (!kIsWeb) {
+      final directory = await getApplicationSupportDirectory();
+      String path = directory.path;
+      final file = await File('$path/userprofile.txt');
+      String data = "Hey hey";
+      // Write the file
+      file.writeAsString(data);
+    }
     return true;
   }
 }
