c8b7af589cda845c8d43c1c818db3ab35817c300
FlutLab.io <>
2022-06-15 12:41:23 +0000

    Introduced User Object for Profile Data

--- lib/model/_user.dart
+++ lib/model/_user.dart
@@ -6,18 +6,45 @@ import 'dart:convert';
 import '../model/_log.dart';
 
 class User {
+  Log log = new Log();
+  Map<String, dynamic>? data;
+
   Future<bool> saveUserData() async {
     String logPrefix = "User | saveUserData";
-    Log log = new Log();
+    log.info(logPrefix, "Starting saveUserData function");
+
+    //Check if this is a browser, in which case we will not save file data
     log.debug(logPrefix, "Checking if this is a browser: " + kIsWeb.toString());
     if (!kIsWeb) {
+      log.debug(logPrefix, "This not a browser. Proceeding to save the file: ");
       final directory = await getApplicationSupportDirectory();
       String path = directory.path;
       final file = await File('$path/userprofile.txt');
-      String data = "Hey hey";
+      String stringData = json.encode(data);
       // Write the file
-      file.writeAsString(data);
+      file.writeAsString(stringData);
+    } else {
+      log.debug(logPrefix, "This is a browser. file will NOT be saved.");
     }
     return true;
+  }
+
+  Future<int> readUserData() async {
+    String logPrefix = "User | readUserData";
+    try {
+      log.debug(logPrefix, "This not a browser. Proceeding to read file Data.");
+      final directory = await getApplicationSupportDirectory();
+      String path = directory.path;
+      final file = await File('$path/userprofile.txt');
+
+      // Read the file
+      final contents = await file.readAsString();
+      data = json.decode(contents.trim());
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
   }
 }
