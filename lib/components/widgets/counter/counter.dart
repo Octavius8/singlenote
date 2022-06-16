eb5a0ff656f318508c468da33f125faaf9c12f16
FlutLab.io <>
2022-06-16 13:35:47 +0000

    Autosave

--- lib/components/widgets/counter/counter.dart
+++ lib/components/widgets/counter/counter.dart
@@ -32,7 +32,7 @@ class CounterState extends State<Counter> {
             width: Config.WIDGET_WIDTH,
             height: Config.WIDGET_HEIGHT,
             child: Column(children: [
-              Text(widget.user.data?['data']['primaryWidgets']), // style: TextStyle(color: Color(int.parse(widget.user.data?['color_highlight'], radix: 16)))),
+              Text(widget.user.data?['data']['name']), // style: TextStyle(color: Color(int.parse(widget.user.data?['color_highlight'], radix: 16)))),
               Text(widget.narration, style: TextStyle(fontSize: Config.WIDGET_FONTSIZE))
             ])));
   }
