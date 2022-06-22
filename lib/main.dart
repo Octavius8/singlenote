894770b270a62821357a039b17385fc7b9a89d84
FlutLab.io <>
2022-06-22 10:52:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -229,7 +229,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                             _lockedScreen ? Text("NOTE | 29", style: TextStyle(fontSize: 32, color: Config.COLOR_DARKGRAY)) : SizedBox.shrink(),
 
                             //Password
-                            SizedBox(width: 50, child: TextField(decoration: InputDecoration(fillColor: Config.COLOR_DARKGRAY))),
+                            SizedBox(width: 100, child: TextField(decoration: InputDecoration(fillColor: Config.COLOR_DARKGRAY, filled: true))),
 
                             //Text(_authorized),
                             GestureDetector(
