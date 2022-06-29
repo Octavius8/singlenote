dd52f7d0672d86ca338c54daef475ad5762f1937
FlutLab.io <>
2022-06-29 10:37:55 +0000

    Autosave

--- lib/components/widgets/countdown/countdown.dart
+++ lib/components/widgets/countdown/countdown.dart
@@ -56,7 +56,7 @@ class CountDownState extends State<CountDown> {
         "name": "Google UK English Female",
         "locale": "en-GB"
       });
-      flutterTts.setPitch(0.3);
+      flutterTts.setPitch(0.7);
       var value = await flutterTts.speak("Countdown Complete");
 
       var _type = FeedbackType.impact;
