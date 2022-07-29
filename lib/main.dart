48f43da727fc271a08f0f8b13f4fdf89c6a19945
FlutLab.io <>
2022-07-29 08:24:09 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -244,7 +244,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         Container(
                           width: double.infinity,
                           height: 50,
-                          padding: 20,
+                          padding: EdgeInsets.all(20),
                           decoration: BoxDecoration(
                             boxShadow: [
                               BoxShadow(
