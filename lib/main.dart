a5741e10942468de2534d7a915f19435bf827f99
FlutLab.io <>
2022-07-29 08:36:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -246,10 +246,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                           width: double.infinity,
                           height: 50,
                           decoration: BoxDecoration(
-                            color: Colors.blue,
+                            color: Config.COLOR_PRIMARY,
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
+                                color: Colors.black,
                                 spreadRadius: 5,
                                 blurRadius: 7,
                                 offset: Offset(0, 3), // changes position of shadow
