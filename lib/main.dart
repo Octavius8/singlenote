7b1dbbc51701d9545b4aa0714f7b57396904f0c9
FlutLab.io <>
2022-07-29 12:57:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -407,8 +407,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     notesList.add(note3);
 
     notesList.forEach((note) {
-      String content = "Lorem Ipsum, Ribababa ...";
-      content = content.substring(0, 20).toString();
+      String content = note.noteContent;
+      content = content.substring(0, 20).toString() + " ...";
       finalList.add(GestureDetector(
           onTap: () {
             _currentView = Config.VIEW_SHOWNOTE;
